28cb5a99cafbb322dfc2c7ccb25741fcf7b635e6
ProphetLamb <rene.carannante@outlook.de>
2021-01-07 20:24:09 +0100

    Add Resources: ContextMenuAddToFavorites

--- EverythingToolbar/Properties/Settings.Designer.cs
+++ EverythingToolbar/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace EverythingToolbar.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -224,6 +224,18 @@ namespace EverythingToolbar.Properties {
             }
             set {
                 this["isHideEmptySearchResults"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute(".\\\\favorites.json")]
+        public string favoritesRelativePathAndFileName {
+            get {
+                return ((string)(this["favoritesRelativePathAndFileName"]));
+            }
+            set {
+                this["favoritesRelativePathAndFileName"] = value;
             }
         }
     }
