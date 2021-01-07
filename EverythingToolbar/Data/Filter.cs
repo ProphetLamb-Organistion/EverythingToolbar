a0c498e135fb700e93952d6d8cbeaccd65007503
ProphetLamb <rene.carannante@outlook.de>
2021-01-07 19:55:37 +0100

    Implement IEquatable<Filter>

--- EverythingToolbar/Data/Filter.cs
+++ EverythingToolbar/Data/Filter.cs
@@ -1,6 +1,8 @@
-﻿namespace EverythingToolbar.Data
+﻿using System;
+
+namespace EverythingToolbar.Data
 {
-    class Filter
+    internal class Filter : IEquatable<Filter>
     {
         public string Name { get; set; }
         public bool IsMatchCase { get; set; }
@@ -12,19 +14,32 @@
 
         public override bool Equals(object obj)
         {
-            Filter item = obj as Filter;
-
-            if (item == null)
-            {
-                return false;
-            }
-
-            return Name.Equals(item.Name);
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Filter) obj);
         }
 
         public override int GetHashCode()
         {
-            return this.Name.GetHashCode();
+            return (Name != null ? Name.GetHashCode() : 0);
+        }
+
+        public bool Equals(Filter other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Name == other.Name;
+        }
+
+        public static bool operator ==(Filter left, Filter right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Filter left, Filter right)
+        {
+            return !Equals(left, right);
         }
     }
 }
