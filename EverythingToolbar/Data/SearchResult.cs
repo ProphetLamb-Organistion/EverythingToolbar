f5950f389dd4a90522594b02b49a3ab408fac425
ProphetLamb <rene.carannante@outlook.de>
2021-01-07 19:57:58 +0100

    Asyncronous IOOps - Centralized IOOps - Aggressive file info loading

--- EverythingToolbar/Data/SearchResult.cs
+++ EverythingToolbar/Data/SearchResult.cs
@@ -1,32 +1,99 @@
 ﻿using EverythingToolbar.Helpers;
 using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 
 namespace EverythingToolbar
 {
-    public class SearchResult
+    /*
+     * 1. IOOps should always be async.
+     * 2. A misconfigured Everything also provides file-paths to which we do not have access, e.g. $RECYCLE_BIN\**\. Especially if "Exclude system files and folders" is not checked in Everything.
+     * 3. Realtime information about the File is not necessary, obtain such when the FullPathAndFileName changed instead of on demand. In fact Murphy's law suggests that a snapshot is likely more coherent in this case.
+     *    Therefore use SetFullPathAndFileName & SetIconSourceFilePath.
+     * 4. If one UnauthorizedAccessException is thrown other IOOps will almost certainly do so as-well, so we save precious cycles waiting for IO and Exception handling by canceling after one Exception.
+     */
+    public class SearchResult : IEquatable<SearchResult>
     {
         public bool IsFile { get; set; }
 
-        public string FullPathAndFileName { get; set; }
+        private string _fullPathAndFileName;
+        public string FullPathAndFileName
+        {
+            get => _fullPathAndFileName;
+            set => SetFullPathAndFileName(value);
+        }
 
-        public string Path => System.IO.Path.GetDirectoryName(FullPathAndFileName);
+        public string Path { get; internal set; }
 
         public string HighlightedPath { get; set; }
 
-        public string FileName => System.IO.Path.GetFileName(FullPathAndFileName);
+        public string FileName { get; internal set; }
 
         public string HighlightedFileName { get; set; }
 
-        public string FileSize => IsFile ? Utils.GetHumanReadableFileSize(FullPathAndFileName) : "";
+        public string FileSize { get; internal set; }
 
-        public string DateModified => File.GetLastWriteTime(FullPathAndFileName).ToString("g");
+        public string DateModified { get; internal set; }
 
-        public ImageSource Icon => WindowsThumbnailProvider.GetThumbnail(FullPathAndFileName, 16, 16);
+        public ImageSource Icon { get; internal set; }
+
+        private string _iconSourceFilePath;
+        public string IconSourceFilePath { get => _iconSourceFilePath; set => SetIconSourceFilePath(value); }
+
+        public Task FetchFileInfo { get; private set; }
+
+        public bool IsFavorite { get; set; }
+
+        private async void SetFullPathAndFileName(string value)
+        {
+            if (FetchFileInfo != null)
+                await FetchFileInfo.ConfigureAwait(true);
+            FetchFileInfo = Task.Run(() =>
+            {
+                if (value is null)
+                {
+                    Path = String.Empty;
+                    FileName = String.Empty;
+                    FileSize = String.Empty;
+                    DateModified = "N/A";
+                    Icon = null;
+                } 
+                else try
+                {
+                    _fullPathAndFileName = value;
+                    Path = System.IO.Path.GetDirectoryName(value);
+                    FileName = System.IO.Path.GetFileName(value);
+                    FileSize = IsFile ? Utils.GetHumanReadableFileSize(value) : String.Empty;
+                    DateModified = File.GetLastWriteTime(value).ToString("g");
+                    Icon = Utils.Catch<ImageSource, Exception>(() =>
+                        WindowsThumbnailProvider.GetThumbnail(IconSourceFilePath ?? value, 16, 16));
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    FileSize = String.Empty;
+                    DateModified = "N/A";
+                    Icon = null;
+                }
+            });
+        }
+
+        private async void SetIconSourceFilePath(string value)
+        {
+            if (FetchFileInfo != null)
+                await FetchFileInfo.ConfigureAwait(true);
+            FetchFileInfo = Task.Run(() =>
+            {
+                _iconSourceFilePath = value;
+                Icon = Utils.Catch<ImageSource, Exception>(() => 
+                    WindowsThumbnailProvider.GetThumbnail(value ?? FullPathAndFileName, 16, 16));
+            });
+        }
 
         public void Open()
         {
