a4d8ebc156e44e450580434f962cd9f824cb4a88
ProphetLamb <rene.carannante@outlook.de>
2021-01-08 15:37:30 +0100

    Fix parallelism

--- EverythingToolbar/Favorites.cs
+++ EverythingToolbar/Favorites.cs
@@ -1,88 +1,144 @@
-﻿using Newtonsoft.Json;
+﻿using CsvHelper;
+using NLog;
+using EverythingToolbar.Data;
+using EverythingToolbar.Helpers;
 
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace EverythingToolbar
 {
-    public class Favorites
+    internal class Favorites
     {
-        internal IList<string> favorites;
+        internal IList<FileListRecord> favorites;
         internal IList<int> favoritesHashes;
-        private static readonly JsonSerializer s_serializer = JsonSerializer.CreateDefault();
+        private readonly ILogger logger;
+        private static readonly SemaphoreSlim fileLock = new SemaphoreSlim(1, 1);
 
-        public Task AddAndWriteAsync(SearchResult item)
+        static Favorites() { }
+
+        public Favorites()
         {
-            favorites.Add(item.FullPathAndFileName);
+            logger = ToolbarLogger.GetLogger("EverythingToolbar");
+        }
+
+        internal string FilePath => Properties.Settings.Default.favoritesRelativePathAndFileName ?? "./favorites.efu";
+
+        public async Task AddAndWriteAsync(SearchResult item)
+        {
+            favorites.Add(await item.ToFileListRecord());
             favoritesHashes.Add(item.FullPathAndFileName.GetHashCode());
-            return Task.Run(() => WriteToDevice(favorites));
+            if (!await WriteToDevice(favorites, FilePath))
+                logger.Error("Failed to write favorites to device. filename: \"{0}\"", Path.GetFullPath(FilePath));
         }
 
-        public Task ReadAsync()
+        public async Task ReadAsync()
         {
-            return Task.Run(async () =>
-            {
-                var favs = ReadFromDevice();
-                CleanDead(favs);
-                var t = Task.Run(() => WriteToDevice(favs)).ConfigureAwait(true);
-                favoritesHashes = favs.Select(x => x.GetHashCode()).ToList();
-                favorites = favs;
-                await t;
-            });
+            var favs = await ReadFromDevice(FilePath);
+            if (favs is null)
+                logger.Error("Failed to read favorites from device. filename: \"{0}\"", Path.GetFullPath(FilePath));
+            await UpdateFileListData(favs);
+            var write = WriteToDevice(favs, FilePath);
+            favoritesHashes = favs.Select(x => x.GetHashCode()).ToList();
+            favorites = favs;
+            if (!await write)
+                logger.Error("Failed to write favorites to device. filename: \"{0}\"", Path.GetFullPath(FilePath));
         }
 
-        public bool Contains(string filePath)
+        public bool Contains(string fileName)
         {
-            if (String.IsNullOrEmpty(filePath))
+            if (String.IsNullOrEmpty(fileName))
                 return false;
-            int hash = filePath.GetHashCode();
+            int hash = fileName.GetHashCode();
             for (int i = 0; i < favoritesHashes.Count; i++)
             {
-                if (hash == favoritesHashes[i] && favorites[i] == filePath)
+                if (hash == favoritesHashes[i] && favorites[i].Filename == fileName)
                     return true;
             }
             return false;
         }
-        
-        private static IList<string> ReadFromDevice()
+
+        private static async Task<IList<FileListRecord>> ReadFromDevice(string path)
         {
-            string path = Properties.Settings.Default.favoritesRelativePathAndFileName ?? "./favorites.json";
             if (!File.Exists(path))
             {
-                return new List<string>();
+                return new List<FileListRecord>();
             }
-            using var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-            using var sr = new StreamReader(fs);
-            using var tr = new JsonTextReader(sr);
-            return s_serializer.Deserialize<List<string>>(tr);
-        }
-        
-        private static void WriteToDevice(IList<string> items)
-        {
-            using var fs = new FileStream(Properties.Settings.Default.favoritesRelativePathAndFileName ?? "./favorites.json", FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
-            using var sr = new StreamWriter(fs);
-            using var tw = new JsonTextWriter(sr);
-            s_serializer.Serialize(tw, items);
+            await fileLock.WaitAsync();
+            try
+            {
+                IList<FileListRecord> records = new List<FileListRecord>();
+                FileListRecord record = new FileListRecord();
+                using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (var sr = new StreamReader(fs))
+                using (var tr = new CsvReader(sr, System.Globalization.CultureInfo.InvariantCulture))
+                {
+                    var en =  tr.EnumerateRecordsAsync<FileListRecord>(record).GetAsyncEnumerator();
+                    while(await en.MoveNextAsync())
+                        records.Add(en.Current);
+                    await en.DisposeAsync();
+                }
+                return records;
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                fileLock.Release();
+            }
         }
 
-        private static void CleanDead(IList<string> items)
+        private static async Task<bool> WriteToDevice(IList<FileListRecord> items, string path)
+        {
+            await fileLock.WaitAsync();
+            try
+            {
+                using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+                using (var sr = new StreamWriter(fs))
+                using (var tw = new CsvWriter(sr, System.Globalization.CultureInfo.InvariantCulture))
+                {
+                    await tw.WriteRecordsAsync(items);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                fileLock.Release();
+            }
+        }
+
+        private static async Task UpdateFileListData(IList<FileListRecord> items)
         {
             for (int i = items.Count-1; i >= 0; i--)
             {
-                int index = i;
-                try
+                FileListRecord item = items[i];
+                FileSystemInfo fsi = null;
+                bool isFile = false;
+                if (isFile = File.Exists(item.Filename))
+                    fsi = await Utils.CatchAsync<FileInfo, UnauthorizedAccessException>(() => new FileInfo(item.Filename));
+                else if (Directory.Exists(item.Filename))
+                    fsi = await Utils.CatchAsync<DirectoryInfo, UnauthorizedAccessException>(() => new DirectoryInfo(item.Filename));
+                if (fsi is null)
                 {
-                    if (!File.Exists(items[index]) && !Directory.Exists(items[index]))
-                    {
-                        items.RemoveAt(index);
-                    }
+                    items.RemoveAt(i);
                 }
-                catch(Exception ex)
+                else
                 {
-                    throw new TaskCanceledException("Unexpected error occured during task execution.", ex);
+                    if (isFile)
+                        item.Size = ((FileInfo)fsi).Length;
+                    item.DateModified = fsi.LastWriteTime.Ticks;
+                    item.DateCreated = fsi.CreationTime.Ticks;
+                    item.Attributes = (int?)fsi.Attributes;
                 }
             }
         }
